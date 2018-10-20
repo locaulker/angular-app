f4f9fc57ff1da475040aa00f4f3fe46b455ccd02
Lawrence Caulker <locaulker@gmail.com>
2018-10-20 07:20:52 -0400

    template syntax section completed

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/User';
 
 @Component({
   selector: 'app-users',
@@ -6,11 +7,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  users: User[];
+  showExtended: boolean = true;
+  loaded: boolean = false;
+  enableAdd: boolean = true;
 
   constructor() { }
 
   ngOnInit() {
-    console.log('init...');
+    // setTimeout(() => {
+      this.users = [
+        {
+          firstName: 'John',
+          lastName: 'Doe',
+          age: 70,
+          address: {
+            street: '50 Main Street',
+            city: 'Boston',
+            state: 'MA'
+          },
+          isActive: true,
+          registered: new Date('01/02/2018 08:30:00')
+        },
+        {
+          firstName: 'Kevin',
+          lastName: 'Johnson',
+          age: 34,
+          address: {
+            street: '20 School Street',
+            city: 'Lynn',
+            state: 'MA'
+          },
+          isActive: false,
+          registered: new Date('03/11/2017 06:20:00')
+        },
+        {
+          firstName: 'Karen',
+          lastName: 'Williams',
+          age: 26,
+          address: {
+            street: '55 Mill Street',
+            city: 'Miami',
+            state: 'FL'
+          },
+          isActive: true,
+          registered: new Date('11/02/2016 10:30:00')
+        }
+      ];
+
+    this.loaded =true;
   }
 
+  addUser(user: User) {
+    this.users.push(user);
+  }
 }
